9df266b151a88c448c53d9c02103ef0f55d4ae79
tianpeilong <101088861+geniusppl@users.noreply.github.com>
2025-02-26 19:29:33 +0800

    Add files via upload

---
+++ generate_subscription.py
@@ -0,0 +1,77 @@
+import pyaes
+import requests
+import base64
+import json
+import pyaes
+import binascii
+from datetime import datetime
+
+print(" \t\tH͜͡E͜͡L͜͡L͜͡O͜͡ \u0295͜͡O͜͡R͜͡L͜͡D͜͡ \u0295͜͡X͜͡T͜͡R͜͡A͜͡C͜͡T͜͡ \u0295͜͡S͜͡ \u0295͜͡N͜͡O͜͡D͡E͡")
+print("𓆝 𓆟 𓆞 𓆟 𓆝 𓆟 𓆞 𓆟 𓆝 𓆟 𓆞 𓆟")
+print("Author : \u0399\u1d7c")
+print(f"Date \t : {datetime.today().strftime('%Y-%m-%d')}")
+print("Version: 1.0")
+print("𓆝 𓆟 𓆞 𓆟 𓆝 𓆟 𓆝 𓆟 𓆞 𓆟 𓆝 𓆟 𓆞 𓆟")
+print("\u0399\u1d7c:")
+print(r"""
+
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠤⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠁⠀⡰⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⡎⢀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀
+⣀⠀⠀⠀⠀⠀⠀⡠⠬⡡⠬⡋⠀⡄⠀⠀⠀⠀⠀⠀⠀
+⡀⠁⠢⡀⠀⠀⢰⠠⢷⠰⠆⡅⠀⡇⠀⠀⠀⣀⠔⠂⡂
+⠱⡀⠀⠈⠒⢄⡸⡑⠊⢒⣂⣦⠄⢃⢀⠔⠈⠀⠀⡰⠁
+⠀⠱⡀⠀⠀⡰⣁⣼⡿⡿⢿⠃⠠⠚⠁⠀⠀⢀⠜⠀⠀
+⠀⠀⠐⢄⠜⠀⠈⠓⠒⠈⠁⠀⠀⠀⠀⠀⡰⠃⠀⠀⠀
+⠀⠀⢀⠊⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠾⡀⠀⠀⠀⠀
+⠀⠀⢸⣄⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡇⠀⠀⠀⠀
+⠀⠀⠸⢸⣳⠦⣍⣁⣀⣀⣀⣀⣠⠴⠚⠁⠇⠀⠀⠀⠀
+⠀⠀⠀⢳⣿⠄⠸⠢⠍⠉⠉⠀⠀⡠⢒⠎⠀⠀⠀⠀⠀
+⠀⠀⠀⠣⣀⠁⠒⡤⠤⢤⠀⠀⠐⠙⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠣⡀⡼⠀⠀⠈⠱⡒⠂⡸⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢒⠁⠀⠀⠀⠀⠀⠀⠀
+""")
+
+
+a = 'http://api.skrapp.net/api/serverlist'
+b = {
+    'accept': '/',
+    'accept-language': 'zh-Hans-CN;q=1, en-CN;q=0.9',
+    'appversion': '1.3.1',
+    'user-agent': 'SkrKK/1.3.1 (iPhone; iOS 13.5; Scale/2.00)',
+    'content-type': 'application/x-www-form-urlencoded',
+    'Cookie': 'PHPSESSID=fnffo1ivhvt0ouo6ebqn86a0d4'
+}
+c = {'data': '4265a9c353cd8624fd2bc7b5d75d2f18b1b5e66ccd37e2dfa628bcb8f73db2f14ba98bc6a1d8d0d1c7ff1ef0823b11264d0addaba2bd6a30bdefe06f4ba994ed'}
+d = b'65151f8d966bf596'
+e = b'88ca0f0ea1ecf975'
+
+
+def f(g, d, e):
+    h = pyaes.AESModeOfOperationCBC(d, iv=e)
+    i = b''.join(h.decrypt(g[j:j+16]) for j in range(0, len(g), 16))
+    return i[:-i[-1]]
+
+
+j = requests.post(a, headers=b, data=c)
+
+if j.status_code == 200:
+    k = j.text.strip()
+    l = binascii.unhexlify(k)
+    m = f(l, d, e)
+    n = json.loads(m)
+    ss_urls = [] # 初始化一个列表来存储ShadowSocks URLs
+    for o in n['data']:
+        p = f"aes-256-cfb:{o['password']}@{o['ip']}:{o['port']}"
+        q = base64.b64encode(p.encode('utf-8')).decode('utf-8')
+        r = f"ss://{q}#{o['title']}"
+        ss_urls.append(r) # 将生成的 ss:// URL 添加到列表中
+
+    subscription_link_content = "\n".join(ss_urls) # 将列表中的 ss:// URL 用换行符连接成一个字符串
+    subscription_link_base64 = base64.b64encode(subscription_link_content.encode('utf-8')).decode('utf-8') # 对整个字符串进行 Base64 编码
+    subscription_link = f"ss://{subscription_link_base64}" # 构造订阅链接，通常以 ss:// 开头，后面跟 Base64 编码的内容
+
+    print("订阅链接如下:") # 打印提示信息
+    print(subscription_link) # 打印最终的订阅链接
+else:
+    print(f"请求失败，状态码: {j.status_code}") # 如果请求失败，打印错误信息
